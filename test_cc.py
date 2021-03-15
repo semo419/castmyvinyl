ce7cd12255a50c6049e1b3fff267cbc45a817cff
Seth Moser <seth.moser@gmail.com>
2021-03-15 05:02:29 +0000

    this is a shitty version I think

--- test_cc.py
+++ test_cc.py
@@ -3,7 +3,8 @@ import time
 import pychromecast 
 
 # Define Chromecasts we're searching for
-target_chromecasts=["Living Room Speaker","Kitchen display"]
+target_chromecasts=["Living Room Speaker","Kitchen display","Office Speaker"]
+target = 2
 
 # List chromecasts on the network, but don't connect
 #services, browser = pychromecast.discovery.discover_chromecasts()
@@ -11,8 +12,7 @@ target_chromecasts=["Living Room Speaker","Kitchen display"]
 #pychromecast.discovery.stop_discovery(browser)
 
 # Discover and connect to chromecasts in target list
-chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=target_chromecasts[1])
-#chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=["Living Room Speaker"])
+chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=[target_chromecasts[target]])
 
 cast = chromecasts[0]
 # Start worker thread and wait for cast device to be ready
@@ -22,16 +22,25 @@ print("Volume = "+str(cast.status.volume_level))
 #print("")
 
 mc = cast.media_controller
-mc.play_media('http://192.168.86.41:8000/rapi.mp3','audio/mp3')
-#mc.play_media('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', 'video/mp4')
+#mc.play_media('http://192.168.86.41:8000/rapi.mp3','audio/mp3')
+mc.play_media('http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4', 'video/mp4')
 mc.block_until_active()
-print(mc.status)
+print(mc.status.player_state)
 
 mc.pause()
 time.sleep(5)
+print(mc.status.player_state)
+
 mc.play()
-time.sleep(5)
-mc.stop()
+time.sleep(10)
+print("Initialized! " + mc.status.player_state)
+
+while mc.status.player_state=="PLAYING":
+	time.sleep(5)
+	print(mc.status.player_state)
+
+#mc.stop()
+print(mc.status.player_state)
 
 # Shut down discovery
 pychromecast.discovery.stop_discovery(browser)

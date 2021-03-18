fbd97c8dfbd20bb8902efe72d9df0fa545b79870
Seth Moser <seth.moser@gmail.com>
2021-03-18 20:25:40 +0000

    code updates

--- test_volknob.py
+++ test_volknob.py
@@ -2,34 +2,44 @@ import RPi.GPIO as GPIO
 import sys
 import time
 
+clk=19
+dt=26
+VoltMeterScale=1 #adjustment factor for output voltage vs. max of voltmeter
+increment=2 #bigger increment makes the volume knob more sensitive
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
 GPIO.setup(21,GPIO.OUT)
-
+GPIO.setup(clk,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(dt,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
 pwm = GPIO.PWM(21,100)
 pwm.start(100)
 
 time.sleep(2)
 
-pwm.ChangeDutyCycle(0)
+pwm.ChangeDutyCycle(50)
 
 time.sleep(2)
 
-v=0
-print(v)
+counter=50
+clkLastState=GPIO.input(clk)
 
 try:
     while True:
-        for v in range (0,100,1):
-            pwm.ChangeDutyCycle(v)
-            print(v)
-            time.sleep(.05)
-        for v in range (100,0,-20):
-            pwm.ChangeDutyCycle(v)
-            print(v)
-            time.sleep(.5)
+        clkState = GPIO.input(clk)
+        dtState = GPIO.input(dt)
+        if clkState != clkLastState:
+            if dtState == clkState and counter < 100:
+                counter += increment
+            elif counter > 0:
+                counter -= increment
+            print(counter)
+            pwm.ChangeDutyCycle(counter*VoltMeterScale)
+        clkLastState = clkState
+        #time.sleep(0.01)
+
 except KeyboardInterrupt:
     print("Ctl C pressed - ending program")
 

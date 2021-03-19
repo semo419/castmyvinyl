14b63675ae62c29710d013bc2426e1aa50acccac
semo419 <62631389+semo419@users.noreply.github.com>
2021-03-19 11:53:53 -0700

    Update CastMyVinyl.py

--- CastMyVinyl.py
+++ CastMyVinyl.py
@@ -116,7 +116,7 @@ def cast_and_monitor(
     print(GPIO.input(button))
 
     #while mc.status.player_state=="PLAYING" and GPIO.input(button)==True:
-    while GPIO.input(button)==True
+    while GPIO.input(button)==True:
         clkState = GPIO.input(clk)
         dtState = GPIO.input(dt)
         if clkState != clkLastState:
